984af29a64b25cac8151451dee0709b65aa70360
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-11-22 14:02:43 -0500

    Update scene_transition_rect.gd

--- StartingOrSafeRooms/scene_transition_rect.gd
+++ StartingOrSafeRooms/scene_transition_rect.gd
@@ -1,22 +1,6 @@
-extends CanvasLayer
+extends Node2D
 
-signal on_transition_finished
+@onready var transition = $Transition
 
-@onready var color_rect = $ColorRect
-@onready var animation_player = $AnimationPlayer
-
-func _ready():
-	color_rect.visible = false
-	animation_player.animation_finished.connect(_on_animation_finished)
-
-
-func _on_animation_finished(anim_name):
-	if anim_name == "Fade":
-		on_transition_finished.emit()
-		animation_player.play("Fade")
-	elif anim_name == "Fade":
-		color_rect.visible = false
-
-func transition():
-	color_rect.visible = true
-	animation_player.play("fade_to_black")
+func on_play_pressed():
+	transition.play("fade_out")
