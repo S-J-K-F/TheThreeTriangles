92681dec07a547ca2b199cbb2e2ae93e621c2893
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-11-22 12:36:15 -0500

    Update scene_transition_rect.gd

--- StartingOrSafeRooms/scene_transition_rect.gd
+++ StartingOrSafeRooms/scene_transition_rect.gd
@@ -1,11 +1,22 @@
-extends ColorRect
+extends CanvasLayer
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	color_rect.visible = false
+	animation_player.animation_finished.connect(_on_animation_finished)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_animation_finished(anim_name):
+	if anim_name == "Fade":
+		on_transition_finished.emit()
+		animation_player.play("fade_to_normal")
+	elif anim_name == "fade_to_normal":
+		color_rect.visible = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func transition():
+	color_rect.visible = true
+	animation_player.play("fade_to_black")
