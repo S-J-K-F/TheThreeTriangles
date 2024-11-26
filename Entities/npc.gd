d82b6642ec7ad567735e58c3b8c862f98124b83f
S-J-K-F <Skey@ftstudent.org>
2024-11-26 13:15:33 -0500

    12

--- Entities/npc.gd
+++ Entities/npc.gd
@@ -3,8 +3,21 @@ extends CharacterBody2D
 const speed = 0
 var current_state = IDLE
 
+var dir = Vector2.RIGHT
+var start_pos
+
+var  is_roaming = true
+var is_chatting = false
+
+var player
+var player_in_chat_zone = false
+
 enum {
 	IDLE,
 	NEW_DIR,
 	Move
 }
+
+func _ready() -> void:
+	randomize()
+	start_pos = position
