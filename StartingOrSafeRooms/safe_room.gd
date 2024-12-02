8bdd63319ea15f455f723306bd0cd0f2d0b6ff97
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-02 12:39:34 -0500

    Update safe_room.gd

--- StartingOrSafeRooms/safe_room.gd
+++ StartingOrSafeRooms/safe_room.gd
@@ -1,5 +1,1 @@
 extends Node2D
-
-func _on_previous_scene_pressed():
-	SceneTransition.load_scene("res://StartingOrSafeRooms/safe_room_2.tsc")
-	#get_tree().change_scene_to_file("StartingOrSafeRooms/safe_room_2.tscn")
