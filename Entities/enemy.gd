78758ca93b49709ac48de5580638f605db564e59
angelaD707 <angeladong22628@gmail.com>
2024-12-03 13:14:59 -0500

    enemy01

--- Entities/enemy.gd
+++ Entities/enemy.gd
@@ -4,31 +4,21 @@ var speed = 25
 var player_chase = false
 var player = null
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	if player_chase:
 		position += (player.position - position)/speed
 		
 		$AnimatedSprite2D.play("walk")
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		 
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.play("left")
+		else:
+			$AnimatedSprite2D.play("right")
+	else: 
+		$AnimatedSprite2D.play("idle")
 
-	move_and_slide()
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
