04021d4e36f1370a6152bcb061dc0f66c81f3d8e
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-04 13:09:37 -0500

    Update dr1.gd

--- DreamRooms/dr1.gd
+++ DreamRooms/dr1.gd
@@ -2,15 +2,15 @@ extends Area2D
 
 var entered = false
 
-func _on_body_entered(body:Node2D) -> void:
+func _on_body_entered(body:PhysicsBody2D) -> void:
 	entered = true
 
 
-func _on_body_exited(body:Node2D) -> void:
+func _on_body_exited(body) -> void:
 	entered = false
 
 
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("ui_select"):
-			get_tree().change_scene_to_file("res://DreamRooms/dream_room.tscn")
+			get_tree().change_scene_to_file("res://DreamRooms/dream_room_2.tscn")
