1c4f920d4ede3186854c3503d50c39a716f0dac4
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-04 12:44:23 -0500

    Update area_2d.gd

--- StartingOrSafeRooms/area_2d.gd
+++ StartingOrSafeRooms/area_2d.gd
@@ -12,5 +12,5 @@ func _on_body_exited(body:Node2D) -> void:
 
 func _process(delta):
 	if entered == true:
-		if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_action_just_pressed("ui_select"):
 			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room.tscn")
