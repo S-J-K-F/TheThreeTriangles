1e13bfb552c21c31f9a60c534b3b386bd21ed2e0
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-06 13:01:44 -0500

    Update sr1.gd

--- StartingOrSafeRooms/sr1.gd
+++ StartingOrSafeRooms/sr1.gd
@@ -4,7 +4,6 @@ var entered = false
 
 func _on_body_entered(body: PhysicsBody2D):
 	entered = true
-	print("test")
 
 func _on_body_exited(body):
 	entered = false
@@ -12,5 +11,5 @@ func _on_body_exited(body):
 
 func _process(delta):
 	if entered == true:
-		if Input.is_action_just_pressed("ui_select"):
+		if Input.is_action_just_pressed("select"):
 			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room.tscn")
