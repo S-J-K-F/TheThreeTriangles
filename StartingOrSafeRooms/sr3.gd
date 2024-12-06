3269080729c69b34bcf93225d1466ead04a64181
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-06 13:14:21 -0500

    Update sr3.gd

--- StartingOrSafeRooms/sr3.gd
+++ StartingOrSafeRooms/sr3.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
+var entered = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+
+func _on_body_exited(body):
+	entered = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("select"):
+			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room_3.tscn")
