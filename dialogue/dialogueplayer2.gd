8fc329db885aaa66fe007ae379ebd49c3761520e
Wolfie-K <Skey24020@gmail.com>
2024-12-11 17:09:05 -0500

    NPC should fully be done

--- dialogue/dialogueplayer2.gd
+++ dialogue/dialogueplayer2.gd
@@ -1,1 +1,6 @@
 extends YapperPlayer
+
+func load_dialogue():
+	var file = FileAccess.open("res://dialogue/NPCTALK_2.json", FileAccess.READ)
+	var content = JSON.parse_string(file.get_as_text())
+	return content
--- dialogue/dialogueplayer5.gd
+++ dialogue/dialogueplayer5.gd
@@ -1,6 +1,6 @@
 extends YapperPlayer
 
 func load_dialogue():
-	var file = FileAccess.open("res://dialogue/NPCTALK_4.json", FileAccess.READ)
+	var file = FileAccess.open("res://dialogue/NPCTALK_5.json", FileAccess.READ)
 	var content = JSON.parse_string(file.get_as_text())
 	return content
