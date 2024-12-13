53597d1fbde08bf5f1bf29c06b6ebae84a21b517
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-13 13:00:35 -0500

    Create dr3.gd

---
+++ DreamRooms/dr3.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body:PhysicsBody2D) -> void:
+	entered = true
+
+
+func _on_body_exited(body) -> void:
+	entered = false
+
+
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("select"):
+
+			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room.tscn")
