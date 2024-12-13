485c2c9edd73d24d7df7b7ac06ed56d681d168ce
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-13 12:55:11 -0500

    Update sr2.gd

--- StartingOrSafeRooms/sr2.gd
+++ StartingOrSafeRooms/sr2.gd
@@ -13,4 +13,4 @@ func _on_body_exited(body) -> void:
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("select"):
-			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room_3.tscn")
+			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room_2.tscn")
