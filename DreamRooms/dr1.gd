91e73f3c9b059813e4cdd243a6a291477f974bf8
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-13 13:00:38 -0500

    Update dr1.gd

--- DreamRooms/dr1.gd
+++ DreamRooms/dr1.gd
@@ -13,5 +13,5 @@ func _on_body_exited(body) -> void:
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("select"):
-			print("hi")
+
 			get_tree().change_scene_to_file("res://DreamRooms/dream_room_2.tscn")
