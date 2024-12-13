9b72eb526188d00824263710c411d6c747186877
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-13 13:03:13 -0500

    Update sr1.gd

--- StartingOrSafeRooms/sr1.gd
+++ StartingOrSafeRooms/sr1.gd
@@ -13,9 +13,5 @@ func _on_body_exited(body) -> void:
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("select"):
-<<<<<<< Updated upstream
 
 			get_tree().change_scene_to_file("res://DreamRooms/dream_room.tscn")
-=======
-			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room.tscn")
->>>>>>> Stashed changes
