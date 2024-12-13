5b4b32db7619bf5fbe224a95773273fc9e081660
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-12-13 13:37:30 -0500

    working transitions😎😎😎😎😎😎😎😎😎
    after two lifetimes of work, things are finally finished

--- NightmareRooms/nd3.gd
+++ NightmareRooms/nd3.gd
@@ -13,4 +13,4 @@ func _on_body_exited(body) -> void:
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("select"):
-			get_tree().change_scene_to_file("res://MediumRooms/medium_room.tscn")
+			get_tree().change_scene_to_file("res://StartingOrSafeRooms/safe_room_3.tscn")
